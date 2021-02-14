824d379d809468d8ea535bdbced7bcf23e08d180
Gota Mitamura <gomitamu@gmail.com>
2021-02-14 16:31:48 +0900

    Var without initializers will be with zero value for each type.

---
+++ zero.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+	var i int
+	var f float64
+	var b bool
+	var s string
+	var u uint
+	var ii int64
+	fmt.Printf("%v, %v, %v, %q, %v, %v", i, f, b, s, u, ii, )
+}
+
